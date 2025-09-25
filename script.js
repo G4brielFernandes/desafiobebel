9b6258f610e7374baf2dfca7ae1f7c8beb20f88c
Francisco Gabriel <gabrielfbarreto.3@gmail.com>
2025-09-25 09:32:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const nome = document.querySelector("#nome")
 const formulario = document.querySelector("#formulario")
 const resultado = document.querySelector("#resultado")
+const email = document.querySelector("#email")
 
 const lista = JSON.parse(localStorage.getItem('dados')) || []
 
 formulario.addEventListener('submit', e => {
     e.preventDefault()
 
-    obj = {nome: nome.value}
+    obj = {
+        nome: nome.value,
+        email: email.value
+    }
     
     lista.push(obj)
     localStorage.setItem('dados', JSON.stringify(lista))
@@ -29,24 +33,31 @@ function montar_card(obj) {
     const del = document.createElement('button')
     const upd = document.createElement('button')
     const br = document.createElement('br')
-    
+    const p2 = document.createElement('p')
+
+    p2.textContent = obj.email
     p.textContent = obj.nome
     del.textContent = 'deletar'
     upd.textContent = 'upadate'
     div.className = 'caixa'
     
-    div.append(p,br,del,upd)
+    div.append(p,p2,br,del,upd)
     resultado.append(div)
 
     upd.addEventListener('click', () => {
         const novo_nome = prompt("Novo nome")
+        const novo_email = prompt("Novo email")
+        
         lista.forEach(e => {
-            if (e.nome === obj.nome){
+            if (e.nome === obj.nome && e.email === obj.email){
                 e.nome = novo_nome
+                e.email = novo_email
             }
+
             p.textContent = novo_nome
+            p2.textContent = novo_email
             localStorage.setItem('dados', JSON.stringify(lista))
-            div.append(p,br,del,upd)
+            div.append(p,p2,br,del,upd)
             resultado.append(div)
         })
     })
@@ -55,7 +66,7 @@ function montar_card(obj) {
         resultado.removeChild(div)
 
         lista.forEach(e => {
-            if (e.nome === obj.nome){
+            if (e.nome === obj.nome && e.email === obj.email){
                 const pos = lista.indexOf(e)
                 lista.splice(pos, 1)
                 localStorage.setItem('dados', JSON.stringify(lista))
